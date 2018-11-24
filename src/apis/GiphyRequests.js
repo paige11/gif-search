a66cca820cee83a9b10967ff8c7c7cd13f370d97
paige11 <ellis.paige.v@gmail.com>
2018-11-24 03:42:39 -0500

    set up folders

--- src/apis/GiphyRequests.js
+++ src/apis/GiphyRequests.js
@@ -5,6 +5,5 @@ const BASE_URL = `https://api.giphy.com/v1/gifs/search?api_key=${PUBLIC_KEY}`;
 
 export const searchForGifs = searchString => {
   let searchParams = searchString.replace(/ /g, '+');
-
   return axios.get(`${BASE_URL}&q=${searchParams}`);
 }
